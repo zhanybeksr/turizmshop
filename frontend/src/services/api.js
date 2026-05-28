adf1496243b04c99a0c25ae92628842c2e268067
Zhanybek Samakov <janybeksamakov@gmail.com>
2026-05-28 16:07:28 +0600

    chore: add production vercel domain to CORS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://turizmshop-backend-2y5m.onrender.com';
+const API_URL = 'https://turizmshop-backend-2y5m.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
